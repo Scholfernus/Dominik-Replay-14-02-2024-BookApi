b6516b69934453a43e930f87a01f07a02ad0d99f
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-15 22:29:01 +0100

    Zmiana w data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO book_model (title, author, year_of_edition) VALUES ('Lord of the ring', 'Tolkien', 1978);
+INSERT INTO book_model (title, author, year_of_edition) VALUES ('War & Peace', 'Tolstoy', 1954);
+INSERT INTO book_model (title, author, year_of_edition) VALUES ('Angels And Demons', 'Brown', 2005);
+INSERT INTO book_model (title, author, year_of_edition) VALUES ('Lost symbol', 'Brown', 2002);
+INSERT INTO book_model (title, author, year_of_edition) VALUES ('Hobbit', 'Tolkien', 2000);
